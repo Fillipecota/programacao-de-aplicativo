5c66c627d9f73deedb248d0fe9ca35d61381bf06
FILLIPE SANT'ANNA COTA <filipe_s_cota@estudante.sc.senai.br>
2024-08-26 21:51:29 -0300

    segunda semana

---
+++ semana02/src/index.ts
@@ -0,0 +1,32 @@
+import Cachorro from "./Cachorro";
+import Dono from "./Dono";
+
+var d1 = new Dono("Fillipe", "(00) 0000-0000");
+var c1 = new Cachorro("whiskylate", "alcool", "branquinha-amarelado", "meio copo", d1);
+
+
+//Modo de fazer sem o contrutor//
+//var c1 = new Cachorro();
+//c1.nome = "whiskylate"
+//c1.raca = "alcool"
+//c1.cor = "branquinha-amarelado"
+//c1.tamanho = "meio copo"
+//var d1 = new Dono();
+//d1.nome = "Fillipe"
+//d1.telefone = "(00) 0000-0000"
+//c1.dono = d1
+//c1.exibirCachorro();
+//c1.exibirDono();
+
+//var c2 = new Cachorro();
+//c2.nome = "ScoobyDoo";
+//c2.raca = "Pastor Alemão";
+//c2.cor = "Marron";
+//c2.tamanho = " grande";
+//var d2 = new Dono();
+//d2.nome = "Salsicha";
+//d2.telefone = "(11) 0000-0000";
+//c2.dono = d2
+//c2.exibirCachorro();
+//c2.exibirDono();
+
