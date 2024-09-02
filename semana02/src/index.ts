1565445642449440959c6fde5844eda03235713a
FILLIPE SANT'ANNA COTA <filipe_s_cota@estudante.sc.senai.br>
2024-09-02 19:26:17 -0300

    detalhes projeto

--- semana02/src/index.ts
+++ semana02/src/index.ts
@@ -2,6 +2,27 @@ import ContaBancaria from "./ContaBancaria";
 import Titular from "./Titular";
 import leia from "readline-sync"
 
+var opcao = leia.keyInSelect(["cadastra", "sacar", "depositar", "consultaSaldo"]);
+
+switch (opcao) {
+
+    case 1:
+
+        break
+    case 2:
+
+        break
+    case 3:
+
+        break
+    case 4:
+
+    default:
+        console.log
+        break
+}
+
+
 var nome = leia.question("Digite nome do usuario: ")
 var cpf = leia.question("Digite o cpf: ")
 var pix = leia.question("Digite  o numero pix: ")
