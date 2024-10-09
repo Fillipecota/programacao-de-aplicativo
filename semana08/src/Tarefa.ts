609b679454b411edf295e8bdf0d2293d5334daac
FILLIPE SANT'ANNA COTA <filipe_s_cota@estudante.sc.senai.br>
2024-10-08 21:51:24 -0300

    aula08

--- semana08/src/Tarefa.ts
+++ semana08/src/Tarefa.ts
@@ -1,27 +1,27 @@
-export default class Tarefa{
-constructor(text: string){
-    this.id = Math.floor(Math.random() * 1000000),
-    this.text =text;
-    this.completed = false;
-}
-     private id: number;
-     private text: string;
-     private completed: boolean;
+export default class Tarefa {
+    constructor(text: string) {
+        this.id = Math.floor(Math.random() * 1000000),
+            this.text = text;
+        this.completed = false;
+    }
+    private id: number;
+    private text: string;
+    private completed: boolean;
 
-     public getId(){
+    public getId() {
         return this.id;
-     }
-     public getText(){
+    }
+    public getText() {
         return this.text;
-     }
-     public getCompleted(){
+    }
+    public getCompleted() {
         return this.completed;
-     }
-    
-    public setText(text:string){
+    }
+
+    public setText(text: string) {
         this.text = text;
     }
-    public setCompleted(completed:boolean){
+    public setCompleted(completed: boolean) {
         this.completed = completed;
-        }
     }
+}
--- semana08/src/window.d.ts
+++ semana08/src/window.d.ts
@@ -1,11 +1,11 @@
-export{};
+export { };
 
-declare global{
-    interface Window{
-        addPeloEnter: (evento: KeyboardEvent) => void;
-        adicionarTarefa: () => void;
-        trocaConcluir: (id: number) => void;
-        deletarTarefa: (id: number) => void;
-        editarTarefa: (id: number) => void;
+declare global {
+  interface Window {
+    addPeloEnter: (evento: KeyboardEvent) => void;
+    adicionarTarefa: () => void;
+    trocaConcluir: (id: number) => void;
+    deletarTarefa: (id: number) => void;
+    editarTarefa: (id: number) => void;
   }
 }
