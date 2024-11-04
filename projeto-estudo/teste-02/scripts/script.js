d73bcda91d8dc55776f7384d96315d2263af09f2
Fellipe <fillipe_s_cota@estudante.sc.senai.br>
2024-11-03 22:36:53 -0300

    teste

---
+++ projeto-estudo/teste-02/scripts/script.js
@@ -0,0 +1,25 @@
+var reserva = [];
+
+function adicionarReserca() {
+    const input = document.getElementById("reserva")
+}
+
+function cancelarReserva() {
+
+}
+
+
+function cancelamentoEmEspera() {
+
+}
+
+function render() {
+
+}
+
+
+function addPeloEnter(evento) {
+    if (evento.key === `Enter`) {
+        adicionar();
+    }
+}
