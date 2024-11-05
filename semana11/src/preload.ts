e56daf1627f26c6855e1493fb20dbdb387230cca
FILLIPE SANT'ANNA COTA <filipe_s_cota@estudante.sc.senai.br>
2024-11-04 21:59:51 -0300

    pagina carro

---
+++ semana11/src/preload.ts
@@ -0,0 +1,13 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { contextBridge, ipcMain, ipcRenderer } from "electron";
+import Veiculo from "./entity/Veiculo";
+
+
+contextBridge.exposeInMainWorld('bancoAPI', {
+    createVeiculo: async (veiculo: Veiculo) => await ipcRenderer.invoke('create', veiculo),
+    findAll: async () => await ipcRenderer.invoke('findAll'),
+    findById: async (id: string) => await ipcRenderer.invoke('findById', id),
+    deletarVeiculo: async (id:string) => await ipcRenderer.invoke('deletarVeiculo',id)
+})
---
+++ semana11/src/repository/VeiculosRepository.ts
@@ -0,0 +1,83 @@
+import { truncate } from "original-fs";
+import { Client } from "pg";
+import Veiculo from "src/entity/Veiculo";
+
+export default class VeiculoRepository {
+    private connection: Client
+
+    constructor() {
+        if (!this.connection) {
+            this.connection = new Client({
+                host: "localhost",
+                port: 5432,
+                database: 'revenda',
+                user: 'postgres',
+                password: 'senai'
+            });
+        }
+    }
+
+    async save(veiculo: Veiculo) {
+        try {
+            this.connection.connect();
+            const sql = "INSERT INTO veiculos (id, modelo, cor, ano, preco, placa, imagem) VALUES ($1, $2, $3, $4, $5, $6, $7)";
+
+            const value = [veiculo.getId(), veiculo.getModelo(), veiculo.getCor(), veiculo.getAno(), veiculo.getPreco(), veiculo.getPlaca(), veiculo.getImagem()]
+
+            await this.connection.query(sql, value);
+        } catch (error) {
+            console.log(error)
+        } finally {
+            this.connection.end();
+            this.connection = null;
+        }
+    }
+
+    async findAll() {
+        try {
+            this.connection.connect();
+            const sql = "SELECT * FROM veiculos WHERE esta_ativo =$1"
+            const result = await this.connection.query(sql,[true]);
+            if (result.rows.length > 0) {
+                return result.rows;
+            } else {
+                console.log("não foi encontrado nenhum valor !")
+                return [];
+            }
+        } catch (error) {
+            console.log(error)
+            return [];
+        } finally {
+            this.connection.end();
+        }
+    }
+
+    async findById(id: string) {
+        try {
+            this.connection.connect();
+            const sql = "SELECT * FROM Veiculo WHERE id = $1";
+            const result = await this.connection.query(sql, [id]);
+            return result.rows;
+        } catch (error) {
+            console.log(error)
+            return []
+        } finally {
+            this.connection.end();
+            this.connection = null;
+        }
+    }
+
+    async delete(id: string) {
+        try {
+            this.connection.connect();
+            // const sql = "DELETE FROM veiculos WHERE id = $1";
+            const sql = " UPDATE s SET nome = esta_ativo =$1 WHERE id = $2"
+            await this.connection.query(sql, [false,id])
+        } catch (error) {
+            console.log(error)
+        } finally {
+            this.connection.end();
+            this.connection = null;
+        }
+    }
+}
