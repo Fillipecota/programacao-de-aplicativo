efe96a371f395815fca1344d2b925759345494d0
FILLIPE SANT'ANNA COTA <filipe_s_cota@estudante.sc.senai.br>
2024-12-05 21:45:57 -0300

    utilizaçao do npx

--- vehicle-store/src/index.ts
+++ vehicle-store/src/index.ts
@@ -5,6 +5,8 @@ import UserController from './service/UserService';
 import Product from './entity/Product';
 import ProductRepository from './repository/ProductRepository';
 import prisma from './prisma';
+import path from 'path';
+import { v4 as uuid } from 'uuid';
 
 declare const LOGIN_PRELOAD_WEBPACK_ENTRY: string;
 declare const LOGIN_WEBPACK_ENTRY: string;
@@ -13,6 +15,11 @@ let mainWindow: BrowserWindow;
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
+
+process.env.PRISMA_QUERY_ENGINE_LIBRARY = path.join(
+  __dirname,
+  'native_modules/client/query_engine-windows.dll.node'
+)
 
 const createWindow = (): void => {
   mainWindow = new BrowserWindow({
@@ -50,25 +57,38 @@ app.on('activate', () => {
 });
 
 ipcMain.handle("create_product", async (_: any, product: any) => {
-  // const { name, category, manufacturer, amount } = product;
-  // const newProduct = new Product(name, category, manufacturer, amount);
-  // await new ProductRepository().save(newProduct);
-
-
-  prisma.usuario.create({
-    data: {
-      id: "123123",
-      name: "fellipe",
-      email: "fellipe@hotmail.com",
-      senha: '123'
-    }
+  const { name, category, manufacturer, amount } = product;
+  const newProduct = new Product(name, category, manufacturer, amount);
+  await new ProductRepository().save(newProduct);
+})
+  
+  
+  ipcMain.handle("find_all_product", async (_: any) => {
+    return await new ProductRepository().findAll();
   })
-})
+  
+  ipcMain.handle("find_amount_by_category", async (_: any) => {
+    return await new ProductRepository().findAmountByCategory();
+  })
 
-ipcMain.handle("find_all_product", async (_: any) => {
-  return await new ProductRepository().findAll();
-})
 
-ipcMain.handle("find_amount_by_category", async (_: any) => {
-  return await new ProductRepository().findAmountByCategory();
-})
+
+
+
+    // demostração de criaçao 
+  //   try {
+  
+  //     await prisma.usuario.create({
+  //       data: {
+  //         id: uuid(),
+  //         name: "fellipe",
+  //         email: `${uuid()}@hotmail.com`,
+  //         senha: '123'
+  //       }
+  //     })
+  //     const usuario = await prisma.usuario.findMany();
+  //     console.log(usuario)
+  //   } catch (err: any) {
+    //     console.log("errouu")
+    //     console.log(err)
+  //   }
